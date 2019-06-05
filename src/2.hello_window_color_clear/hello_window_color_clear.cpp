eeb90992514f2022e0e7ec80f1f321d122153447
Rinkowei <282633894@qq.com>
2019-06-05 22:00:08 +0800

    added 2.hello_window_color_clear project

---
+++ src/2.hello_window_color_clear/hello_window_color_clear.cpp
@@ -0,0 +1,69 @@
+﻿
+#define GLFW_INCLUDE_ES32
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+using namespace std;
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void handleInput(GLFWwindow* window);
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+int main()
+{
+	// initialize and configure glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_ES_API, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGLES_Examples", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	if (!gladLoadGLES2Loader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+	else
+	{
+		cout << "Success to initialize GLAD\n";
+	}
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		handleInput(window);
+		
+		// clear color buffer
+		glClearColor(0.7f, 0.3f, 0.6f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// swap buffers and poll IO events(keys pressed / released. mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
+
+void handleInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
