68d1778d2a2d0215a1082a62b094d41b6acfaff9
Rinkowei <282633894@qq.com>
2019-06-30 23:44:30 +0800

    added 11.cull_face project

---
+++ src/11.cull_face/cull_face.cpp
@@ -0,0 +1,160 @@
+﻿
+#define GLFW_INCLUDE_ES32
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <Common/camera.h>
+#include <Common/shader.h>
+#include <Common/textureLoader.h>
+#include <Common/model.h>
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void mouseCallback(GLFWwindow* window, double xPos, double yPos);
+void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+void handleInput(GLFWwindow* window);
+
+const GLuint SCREEN_WIDTH = 800;
+const GLuint SCREEN_HEIGHT = 600;
+
+const string resources_dir(ES_EXAMPLE_RESOURCES_DIR);
+
+Camera camera((float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f, glm::vec3(0.0f, 1.0f, 3.0f));
+float lastX = SCREEN_WIDTH / 2.0f;
+float lastY = SCREEN_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+float deltaTime = 0.0f;
+float lastTime = 0.0f;
+
+int main()
+{
+	// initialize and configure glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_ES_API, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGLES_Examples", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	glfwSetScrollCallback(window, scrollCallback);
+	
+	// capture mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	
+	if (!gladLoadGLES2Loader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+	else
+	{
+		cout << "Success to initialize GLAD\n";
+	}
+
+	// enable cull face that is happen before rasterization, the back face is counter-clockwise
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
+
+	// enable depth test
+	glEnable(GL_DEPTH_TEST);
+	
+	Shader shader(resources_dir + "shaders/10.model_loading/model.vs", resources_dir + "shaders/10.model_loading/model.fs");
+	
+	Model venusModel(resources_dir + "models/carnage/carnage.obj");
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastTime;
+		lastTime = currentFrame;
+
+		handleInput(window);
+		
+		// clear color buffer
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shader.setMat4("projection", camera.getPerspectiveMatrix());
+		shader.setMat4("view", camera.getViewMatrix());
+		
+		// initialize matrix to identity matrix
+		glm::mat4 model = glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+		model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
+
+		shader.setMat4("model", model);
+		shader.use();
+
+		venusModel.Draw(shader);
+
+		// swap buffers and poll IO events(keys pressed / released. mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
+	return 0;
+}
+
+void handleInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.handleKeyboard(Camera::CameraMovement::FORWARD, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.handleKeyboard(Camera::CameraMovement::BACKWARD, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.handleKeyboard(Camera::CameraMovement::LEFT, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.handleKeyboard(Camera::CameraMovement::RIGHT, deltaTime);
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void mouseCallback(GLFWwindow* window, double xPos, double yPos)
+{
+	if (firstMouse)
+	{
+		lastX = xPos;
+		lastY = yPos;
+		firstMouse = false;
+	}
+	
+	float xOffset = xPos - lastX;
+	float yOffset = yPos - lastY;
+
+	lastX = xPos;
+	lastY = yPos;
+
+	camera.handleMouseMovement(xOffset, -yOffset);
+}
+
+void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
+{
+	camera.handleMouseScroll(yOffset);
+}
