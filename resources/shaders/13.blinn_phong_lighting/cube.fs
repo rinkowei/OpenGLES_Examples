ee5df7518f07e3a3f7d6a6a4c6524ca9b93e93a9
Rinkowei <282633894@qq.com>
2019-07-05 12:49:29 +0800

    added annotation for shader

--- resources/shaders/13.blinn_phong_lighting/cube.fs
+++ resources/shaders/13.blinn_phong_lighting/cube.fs
@@ -56,8 +56,10 @@ vec3 calculateDirectionalLight(DirectionalLight light, vec3 normal, vec3 viewDir
 {
 	vec3 lightDir = normalize(-light.direction);
 
+	// diff irradiance
 	float diff = max(dot(normal, lightDir), 0.0f);
 
+	// spec irradiance
     vec3 halfwayVec = normalize(lightDir + viewDir);
 	float spec = pow(max(dot(normal, halfwayVec), 0.0f), shininess);
 
@@ -65,6 +67,7 @@ vec3 calculateDirectionalLight(DirectionalLight light, vec3 normal, vec3 viewDir
 	vec3 diffuse = light.diffuse * diff * vec3(texture(diffuseMap, fs_in.fTexCoord));
 	vec3 specular = light.specular * spec * vec3(texture(specularMap, fs_in.fTexCoord));
 
+	// combine results
 	return (ambient + diffuse + specular);
 }
 
@@ -72,8 +75,10 @@ vec3 calculatePointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewD
 {
 	vec3 lightDir = normalize(light.position - fragPos);
 
+	// diff irradiance
 	float diff = max(dot(normal, lightDir), 0.0f);
 
+	// spec irradiance
     vec3 halfwayVec = normalize(lightDir + viewDir);
 	float spec = pow(max(dot(normal, halfwayVec), 0.0f), shininess);
 
@@ -88,6 +93,7 @@ vec3 calculatePointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewD
 	diffuse *= attenuation;
 	specular *= attenuation;
 
+	// combine results
 	return (ambient + diffuse + specular);
 }
 
@@ -95,8 +101,10 @@ vec3 calculateSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir
 {
 	vec3 lightDir = normalize(light.position - fragPos);
 
+	// diff irradiance
 	float diff = max(dot(normal, lightDir), 0.0f);
 
+	// spec irradiance
     vec3 halfwayVec = normalize(lightDir + viewDir);
 	float spec = pow(max(dot(normal, halfwayVec), 0.0f), shininess);
 
@@ -115,6 +123,7 @@ vec3 calculateSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir
     diffuse *= attenuation * intensity;
     specular *= attenuation * intensity;
 
+    // combine results
     return (ambient + diffuse + specular);
 }
 
@@ -122,9 +131,8 @@ void main()
 {
     vec3 viewDir = normalize(viewPos - fs_in.fFragPos);
 	vec3 normal = normalize(fs_in.fNormal);
-    vec3 diffuseColor = vec3(texture(diffuseMap, fs_in.fTexCoord));
-    vec3 specularColor = vec3(texture(specularMap, fs_in.fTexCoord));
 
+	// combine all the light sources
 	vec3 result = calculateDirectionalLight(dirLight, normal, viewDir);
 	result += calculatePointLight(pointLight, normal, fs_in.fFragPos, viewDir);
 	result += calculateSpotLight(spotLight, normal, fs_in.fFragPos, viewDir);   
