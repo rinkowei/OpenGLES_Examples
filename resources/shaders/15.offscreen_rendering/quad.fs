56815bdfa7fdc06026cab479c107aefe518429b7
Rinkowei <282633894@qq.com>
2019-07-06 23:46:38 +0800

    added 15.offscreen_rendering project

---
+++ resources/shaders/15.offscreen_rendering/quad.fs
@@ -0,0 +1,8 @@
+#version 320 es
+precision mediump float;
+layout(location = 0) out vec4 fragColor;
+
+void main()
+{
+    fragColor = vec4(0.8f, 0.2f, 0.3f, 0.2f);
+}
---
+++ src/15.offscreen_rendering/offscreen_rendering.cpp
@@ -0,0 +1,219 @@
+﻿
+#define GLFW_INCLUDE_ES32
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <Common/camera.h>
+#include <Common/shader.h>
+#include <Common/textureLoader.h>
+#include <Common/model.h>
+
+#include <iostream>
+#include <array>
+#include <map>
+using namespace std;
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void mouseCallback(GLFWwindow* window, double xPos, double yPos);
+void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+void handleInput(GLFWwindow* window);
+
+const GLuint SCREEN_WIDTH = 800;
+const GLuint SCREEN_HEIGHT = 600;
+
+const string resources_dir(ES_EXAMPLE_RESOURCES_DIR);
+
+Camera camera((float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 10.0f, glm::vec3(0.0f, 0.0f, 2.0f));
+float lastX = SCREEN_WIDTH / 2.0f;
+float lastY = SCREEN_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+float deltaTime = 0.0f;
+float lastTime = 0.0f;
+
+GLuint quadVAO = 0;
+GLuint quadVBO = 0;
+
+void drawQuad()
+{
+	if (quadVAO == 0)
+	{
+		GLfloat vertices[] = {
+			// positions        // texture coordinates
+			0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+			0.0f, -0.5f,  0.0f,  0.0f,  1.0f,
+			1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+
+			0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+			1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+			1.0f,  0.5f,  0.0f,  1.0f,  0.0f
+		};
+
+		glGenVertexArrays(1, &quadVAO);
+		glGenBuffers(1, &quadVBO);
+		
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+
+	glBindVertexArray(quadVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(0);
+}
+
+int main()
+{
+	// initialize and configure glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_ES_API, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGLES_Examples", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	glfwSetScrollCallback(window, scrollCallback);
+	
+	// capture mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	
+	if (!gladLoadGLES2Loader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+	else
+	{
+		cout << "Success to initialize GLAD\n";
+	}
+
+	// enable depth test
+	glEnable(GL_DEPTH_TEST);
+
+	// enbale blend
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBlendEquation(GL_FUNC_ADD);
+	
+	Shader quadShader(resources_dir + "shaders/14.blending/quad.vs", resources_dir + "shaders/14.blending/quad.fs");
+
+	std::array<glm::vec3, 6> quadPositions
+	{
+		glm::vec3(-1.5f, 0.0f, -0.48f),
+		glm::vec3(1.5f, 0.0f, 0.51f),
+		glm::vec3(0.0f, 0.0f, 0.7f),
+		glm::vec3(0.5f, 0.0f, -0.6f),
+		glm::vec3(0.2f, 0.0f, -0.9f),
+		glm::vec3(-0.2f, 0.0f, -1.4f)
+	};
+	
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastTime;
+		lastTime = currentFrame;
+
+		handleInput(window);
+		
+		// clear color buffer
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		std::map<float, glm::vec3> sortedMap;
+		for (unsigned int i = 0; i < quadPositions.size(); i++)
+		{
+			float distance = glm::length(camera.getPosition() - quadPositions[i]);
+			sortedMap[distance] = quadPositions[i];
+		}
+		
+		quadShader.use();
+		quadShader.setMat4("view", camera.getViewMatrix());
+		quadShader.setMat4("projection", camera.getPerspectiveMatrix());
+		
+		// render quadrangles from furthest to nearest
+		glm::mat4 model = glm::mat4(1.0f);
+		for (auto iter = sortedMap.rbegin(); iter != sortedMap.rend(); iter++)
+		{
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, iter->second);
+			quadShader.setMat4("model", model);
+
+			drawQuad();
+		}
+
+		// swap buffers and poll IO events(keys pressed / released. mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &quadVAO);
+	glDeleteBuffers(1, &quadVBO);
+
+	glfwTerminate();
+	return 0;
+}
+
+void handleInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.handleKeyboard(Camera::CameraMovement::FORWARD, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.handleKeyboard(Camera::CameraMovement::BACKWARD, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.handleKeyboard(Camera::CameraMovement::LEFT, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.handleKeyboard(Camera::CameraMovement::RIGHT, deltaTime);
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void mouseCallback(GLFWwindow* window, double xPos, double yPos)
+{
+	if (firstMouse)
+	{
+		lastX = xPos;
+		lastY = yPos;
+		firstMouse = false;
+	}
+	
+	float xOffset = xPos - lastX;
+	float yOffset = yPos - lastY;
+
+	lastX = xPos;
+	lastY = yPos;
+
+	camera.handleMouseMovement(xOffset, -yOffset);
+}
+
+void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
+{
+	camera.handleMouseScroll(yOffset);
+}
