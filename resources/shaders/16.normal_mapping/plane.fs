340b34e65bb740065e49cbe6095a198a1b357fcf
Rinkowei <282633894@qq.com>
2019-07-11 01:06:06 +0800

    added new model resources

--- resources/shaders/16.normal_mapping/plane.fs
+++ resources/shaders/16.normal_mapping/plane.fs
@@ -4,9 +4,11 @@ layout(location = 0) out vec4 fragColor;
 
 in vec2 fTexCoord;
 
-uniform sampler2D textureDiffuse1;
+uniform sampler2D normalMap;
 
 void main()
 {
-    fragColor = texture(textureDiffuse1, fTexCoord);
+    vec3 normal = texture(normalMap, fTexCoord).rgb;
+    normal = normalize(normal * 2.0f - 1.0f);
+    fragColor = vec4(normal, 1.0f);
 }
