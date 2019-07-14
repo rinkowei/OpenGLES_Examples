fb06176741ac138172ca7432bb368b46c5c72f84
Rinkowei <282633894@qq.com>
2019-07-14 20:35:42 +0800

    adjusted Common library structure

---
+++ include/Common/common.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include "camera.h"
+#include "textureLoader.h"
+#include "shader.h"
+#include "mesh.h"
+#include "model.h"
--- src/Common.cpp
+++ src/Common.cpp
@@ -1,9 +1,3 @@
 ﻿// Implementation of Common library
 
-#define STB_IMAGE_IMPLEMENTATION
-
-#include <Common/camera.h>
-#include <Common/shader.h>
-#include <Common/textureLoader.h>
-#include <Common/mesh.h>
-#include <Common/model.h>
+#include <common/common.h>
