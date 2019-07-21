8612ca307b7f5ddf8f3dd390fe9178d639739270
Rinkowei <282633894@qq.com>
2019-07-21 11:21:59 +0800

    updated 17.dear_imgui project

--- src/17.dear_imgui/dear_imgui.cpp
+++ src/17.dear_imgui/dear_imgui.cpp
@@ -6,25 +6,74 @@ class Example : public ExampleBase
 public:
 	Example()
 	{
-
+		defaultClearColor = glm::vec4(0.45f, 0.55f, 0.60f, 1.00f);
 	}
 	~Example()
 	{
 
 	}
 public:
+	bool show_demo_window = true;
+	bool show_another_window = false;
+
 	virtual void render()
 	{
+		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
 
+		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+
+			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float*)& defaultClearColor); // Edit 3 floats representing a color
+
+			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+		}
+		// 3. Show another simple window.
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
+			ImGui::End();
+		}
 	}
 };
 
-Example *example;																																							\
+Example *example;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)						
+{																									
+if (example != nullptr)
+{																								
+	example->handleMessages(hWnd, uMsg, wParam, lParam);
+}																								
+return (DefWindowProc(hWnd, uMsg, wParam, lParam));												
+}																									
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)						
 {			
-	example = new Example();															
-    example->setupGLFW();
-	example->loadGLESFunctions();
+	example = new Example();
+	if (!example->setupGLFW() ||
+		!example->loadGLESFunctions() ||
+		!example->setupImGui())
+	{
+		return 0;
+	}
 	example->setupWindow();													            
 	example->prepare();																	
 	example->renderLoop();																
