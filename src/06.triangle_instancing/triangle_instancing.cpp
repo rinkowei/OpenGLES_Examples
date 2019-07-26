97f0e5fc638bb413be35ccd6fd610062f7134548
Rinkowei <282633894@qq.com>
2019-07-27 00:20:03 +0800

    updated 06.triangle_instancing project

--- src/06.triangle_instancing/triangle_instancing.cpp
+++ src/06.triangle_instancing/triangle_instancing.cpp
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 #define GLFW_INCLUDE_ES32
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -137,3 +137,117 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
+*/
+
+
+#include <common.h>
+using namespace es;
+
+class Example final : public ExampleBase
+{
+public:
+	GLuint VAO, VBO, EBO, instanceVBO;
+	Shader* shader;
+	Example()
+	{
+		title = "triangle instancing";
+		defaultClearColor = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+		shadersPath = getResourcesPath(ResourceType::Shader) + "/06.triangle_instancing/";
+	}
+	~Example()
+	{
+		delete(shader);
+		glDeleteBuffers(1, &instanceVBO);
+		glDeleteBuffers(1, &EBO);
+		glDeleteBuffers(1, &VBO);
+		glDeleteVertexArrays(1, &VAO);
+	}
+public:
+	virtual void prepare() override
+	{
+		// create a array of 100 triangle locations
+		std::array<glm::vec2, 100> locations;
+		unsigned int index = 0;
+		float offset = 0.1f;
+		for (int x = -10; x < 10; x += 2)
+		{
+			for (int y = -10; y < 10; y += 2)
+			{
+				glm::vec2 position((float)x / 10.0f + offset, (float)y / 10.0f + offset);
+				locations[index++] = position;
+			}
+		}
+
+		std::vector<GLfloat> vertexAttrs = {
+			// positions           // colors
+			 0.05f, -0.05f, 0.0f,   1.0f, 0.0f, 0.0f, 1.0f,
+			-0.05f, -0.05f, 0.0f,   0.0f, 1.0f, 0.0f, 1.0f,
+			-0.05f,  0.05f, 0.0f,   0.0f, 0.0f, 1.0f, 1.0f,
+		};
+
+		std::vector<GLuint> indices = {
+			0, 1, 2
+		};
+
+		glGenBuffers(1, &instanceVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * locations.size(), locations.data(), GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertexAttrs.size() * sizeof(GLfloat), vertexAttrs.data(), GL_STATIC_DRAW);
+		
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
+
+		glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(2);
+		glVertexAttribDivisor(2, 1);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
+		// create triangle shader
+		shader = Shader::createWithFile(shadersPath + "triangle.vs", shadersPath + "triangle.fs");
+	}
+	virtual void render() override
+	{
+		shader->use();
+		glBindVertexArray(VAO);
+		glDrawElementsInstanced(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0, 100);
+	}
+};
+
+Example* example;
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
+{
+	example = new Example();
+	example->setupValidation();
+	if (!example->setupGLFW() ||
+		!example->loadGLESFunctions() ||
+		!example->setupImGui())
+	{
+		return 0;
+	}
+	example->prepare();
+	example->renderLoop();
+	delete(example);
+	return 0;
+}
+
+
+
+
