2cfbcf8def86b1dd5798ec1006a08cb3fd2a05b9
Rinkowei <282633894@qq.com>
2019-08-03 02:18:10 +0800

    updated 09.transformations project

--- src/09.transformations/transformations.cpp
+++ src/09.transformations/transformations.cpp
@@ -1,227 +1,144 @@
 ﻿
-#define GLFW_INCLUDE_ES32
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
 #include <common.h>
+using namespace es;
 
-#include <iostream>
-#include <array>
-using namespace std;
-
-void framebufferSizeCallback(GLFWwindow* window, int width, int height);
-void mouseCallback(GLFWwindow* window, double xPos, double yPos);
-void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
-void handleInput(GLFWwindow* window);
-
-const GLuint SCREEN_WIDTH = 800;
-const GLuint SCREEN_HEIGHT = 600;
-
-const string resources_dir(ES_EXAMPLE_RESOURCES_DIR);
-
-Camera camera((float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 10.0f, glm::vec3(0.0f, 0.0f, 7.0f));
-float lastX = SCREEN_WIDTH / 2.0f;
-float lastY = SCREEN_HEIGHT / 2.0f;
-bool firstMouse = true;
-
-float deltaTime = 0.0f;
-float lastTime = 0.0f;
-
-int main()
+class Example final : public ExampleBase
 {
-	// initialize and configure glfw
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_ES_API, GLFW_OPENGL_CORE_PROFILE);
-	
-	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGLES_Examples", nullptr, nullptr);
-	if (window == nullptr)
+public:
+	Mesh* cube;
+	Material* material;
+	Example()
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
+		title = "transformations";
+		defaultClearColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+		shadersDirectory = getResourcesPath(ResourceType::Shader) + "/09.transformations/";
+		texturesDirectory = getResourcesPath(ResourceType::Texture) + "/09.transformations/";
 	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-	glfwSetCursorPosCallback(window, mouseCallback);
-	glfwSetScrollCallback(window, scrollCallback);
-	
-	// capture mouse
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	
-	if (!gladLoadGLES2Loader((GLADloadproc)glfwGetProcAddress))
+	~Example()
 	{
-		cout << "Failed to initialize GLAD\n";
-		return -1;
+		delete(cube);
 	}
-	else
+public:
+	virtual void prepare() override
 	{
-		cout << "Success to initialize GLAD\n";
+		// setup camera
+		camera.type = Camera::Type::firstPerson;
+		camera.rotationSpeed = 0.5f;
+		camera.setPosition(glm::vec3(0.0f, 0.0f, -4.0f));
+
+		glEnable(GL_DEPTH_TEST);
+
+		std::vector<GLfloat> vertexAttrs = {
+			// positions         // texture coordinates
+		   -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		   -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		   -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		};
+
+		std::vector<Vertex> vertices = {};
+		for (uint32_t i = 0; i < static_cast<uint32_t>(vertexAttrs.size() / 5); i++)
+		{
+			Vertex vertex;
+			vertex.Position = glm::vec3(vertexAttrs[i * 5], vertexAttrs[i * 5 + 1], vertexAttrs[i * 5 + 2]);
+			vertex.TexCoords = glm::vec2(vertexAttrs[i * 5 + 3], vertexAttrs[i * 5 + 4]);
+			vertices.push_back(vertex);
+		}
+
+		std::unordered_map<Material::ShaderType, std::string> shaderPaths =
+		{
+			{ Material::ShaderType::Vertex, shadersDirectory + "cube.vert" },
+			{ Material::ShaderType::Fragment, shadersDirectory + "cube.frag" }
+		};
+
+		std::vector<std::pair<Texture::Type, std::string>> texturePaths =
+		{
+			std::make_pair(Texture::Type::Diffuse, texturesDirectory + "face.png")
+		};
+
+		// create quad material
+		material = Material::createWithFile(shaderPaths, texturePaths);
+
+		cube = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, material);
 	}
-
-	glEnable(GL_DEPTH_TEST);
-	
-	Shader* shader = Shader::createWithFile(resources_dir + "shaders/9.transformations/cube.vs", resources_dir + "shaders/9.transformations/cube.fs");
-	
-	GLfloat vertices[] = {
-		 // positions         // texture coordinates
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-	
-	GLuint VAO, VBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	GLuint cubeTextureID = textureLoader::loadTexture2D(resources_dir + "textures/9.transformations/face.png", true);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, cubeTextureID);
-
-	// render loop
-	while (!glfwWindowShouldClose(window))
+	virtual void render() override
 	{
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastTime;
-		lastTime = currentFrame;
+		if (!paused || camera.updated)
+		{
+			// initialize matrix to identity matrix
+			glm::mat4 model = glm::mat4(1.0f);
 
-		handleInput(window);
-		
-		// clear color buffer
-		glClearColor(0.2f, 0.8f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			// translation
+			model = glm::translate(model, glm::vec3(0.1f, 0.4f, 0.3f));
 
-		shader->setMat4("projection", camera.getPerspectiveMatrix());
-		shader->setMat4("view", camera.getViewMatrix());
-		
-		// initialize matrix to identity matrix
-		glm::mat4 model = glm::mat4(1.0f);
-		// translation
-		model = glm::translate(model, glm::vec3(0.2f, 0.5f, 0.3f));
-		
-		// rotation
-		model = glm::rotate(model, currentFrame * glm::radians(90.0f), glm::vec3(0.5f, 0.2f, 0.8f));
+			// rotation
+			model = glm::rotate(model, timePassed * 2.0f * glm::radians(90.0f), glm::vec3(0.5f, 0.2f, 0.8f));
 
-		// scale
-		float scale = glm::max(glm::sin(currentFrame), 0.2f);
-		model = glm::scale(model, glm::vec3(scale));
+			// scale
+			float scale = glm::max(glm::sin((double)timePassed * 2.0), 0.3);
+			model = glm::scale(model, glm::vec3(scale));
 
-		shader->setMat4("model", model);
-		shader->use();
+			material->setMat4("model", model);
+			material->setMat4("view", camera.matrices.view);
+			material->setMat4("projection", camera.matrices.projection);
+		}
 
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		// swap buffers and poll IO events(keys pressed / released. mouse moved etc.)
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		// render quad mesh
+		cube->Draw();
 	}
+};
 
-	// delete textures and buffers
-	glDeleteTextures(1, &cubeTextureID);
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	
-	glfwTerminate();
+Example* example;
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
+{
+	example = new Example();
+	example->setupValidation();
+	if (!example->setupGLFW() ||
+		!example->loadGLESFunctions() ||
+		!example->setupImGui())
+	{
+		return 0;
+	}
+	example->prepare();
+	example->renderLoop();
+	delete(example);
 	return 0;
-}
-
-void handleInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-	else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.handleKeyboard(Camera::CameraMovement::FORWARD, deltaTime);
-	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.handleKeyboard(Camera::CameraMovement::BACKWARD, deltaTime);
-	else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.handleKeyboard(Camera::CameraMovement::LEFT, deltaTime);
-	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.handleKeyboard(Camera::CameraMovement::RIGHT, deltaTime);
-}
-
-void framebufferSizeCallback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-void mouseCallback(GLFWwindow* window, double xPos, double yPos)
-{
-	if (firstMouse)
-	{
-		lastX = xPos;
-		lastY = yPos;
-		firstMouse = false;
-	}
-	
-	float xOffset = xPos - lastX;
-	float yOffset = yPos - lastY;
-
-	lastX = xPos;
-	lastY = yPos;
-
-	camera.handleMouseMovement(xOffset, -yOffset);
-}
-
-void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
-{
-	camera.handleMouseScroll(yOffset);
 }
