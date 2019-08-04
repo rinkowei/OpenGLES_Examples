292dbffe556a346ed7c0ffb24d01d76fea2e8b20
Rinkowei <282633894@qq.com>
2019-08-04 22:41:49 +0800

    updated 10.model_loading project

--- common/model.h
+++ common/model.h
@@ -19,188 +19,190 @@
 #include <common.h>
 
 using namespace std;
-using namespace es;
 
-class Model
+namespace es
 {
-public:
-	Model() = default;
-	~Model() = default;
-
-	static Model* createWithFile(const std::string& path, bool gamma = false)
+	class Model
 	{
-		Model* model = new (std::nothrow) Model();
-		if (model && model->loadWithFile(path, gamma))
+	public:
+		Model() = default;
+		~Model() = default;
+
+		static Model* createWithFile(const std::string& path, bool gamma = false)
 		{
-			return model;
-		}
-		delete(model);
-		return nullptr;
-	}
-
-	void Draw(Shader shader)
-	{
-		for (unsigned int i = 0; i < meshes.size(); i++)
-		{
-			meshes[i].Draw();
-		}
-	}
-
-private:
-	std::vector<Texture*> texturesLoaded;
-	std::vector<Mesh> meshes;
-
-	std::string directory;
-	bool gammaCorrection = false;
-
-	bool loadWithFile(const std::string& path, bool gamma)
-	{
-		Assimp::Importer importer;
-		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace | aiProcess_PreTransformVertices);
-		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
-		{
-			std::cout << importer.GetErrorString() << std::endl;
-			return false;
-		}
-		directory = path.substr(0, path.find_last_of('/'));
-
-		handleNode(scene->mRootNode, scene);
-
-		return true;
-	}
-
-	void handleNode(aiNode* node, const aiScene* scene)
-	{
-		for (unsigned int i = 0; i < node->mNumMeshes; i++)
-		{
-			aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-			meshes.push_back(handleMesh(mesh, scene));
+			Model* model = new (std::nothrow) Model();
+			if (model && model->loadWithFile(path, gamma))
+			{
+				return model;
+			}
+			delete(model);
+			return nullptr;
 		}
 
-		for (unsigned int i = 0; i < node->mNumChildren; i++)
+		void Draw(Shader shader)
 		{
-			handleNode(node->mChildren[i], scene);
-		}
-	}
-
-	Mesh handleMesh(aiMesh* mesh, const aiScene* scene)
-	{
-		std::vector<Vertex> vertices;
-		std::vector<GLuint> indices;
-		std::vector<Texture*> textures;
-	
-		for (unsigned int i = 0; i < mesh->mNumVertices; i++)
-		{
-			Vertex vertex;
-			
-			if (mesh->HasPositions())
+			for (unsigned int i = 0; i < meshes.size(); i++)
 			{
-				vertex.Position = glm::vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z);
+				meshes[i].Draw();
 			}
-			else
-			{
-				vertex.Position = glm::vec3(0.0f);
-			}
-
-			if (mesh->HasNormals())
-			{
-				vertex.Normal = glm::vec3(mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z);
-			}
-			else
-			{
-				vertex.Normal = glm::vec3(0.0f);
-			}
-
-			if (mesh->HasTextureCoords(0))
-			{
-				vertex.TexCoords = glm::vec2(mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y);
-			}
-			else
-			{
-				vertex.TexCoords = glm::vec2(0.0f);
-			}
-
-			if (mesh->HasTangentsAndBitangents())
-			{
-				vertex.Tangent = glm::vec3(mesh->mTangents[i].x, mesh->mTangents[i].y, mesh->mTangents[i].z);
-				vertex.Bitangent = glm::vec3(mesh->mBitangents[i].x, mesh->mBitangents[i].y, mesh->mBitangents[i].z);
-			}
-			else
-			{
-				vertex.Tangent = glm::vec3(0.0f);
-				vertex.Bitangent = glm::vec3(0.0f);
-			}
-			
-			vertices.push_back(vertex);
 		}
 
-		if (mesh->HasFaces())
+	private:
+		std::vector<Texture*> texturesLoaded;
+		std::vector<Mesh> meshes;
+
+		std::string directory;
+		bool gammaCorrection = false;
+
+		bool loadWithFile(const std::string& path, bool gamma)
 		{
-			for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+			Assimp::Importer importer;
+			const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace | aiProcess_PreTransformVertices);
+			if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
 			{
-				aiFace face = mesh->mFaces[i];
-				for (unsigned int j = 0; j < face.mNumIndices; j++)
+				std::cout << importer.GetErrorString() << std::endl;
+				return false;
+			}
+			directory = path.substr(0, path.find_last_of('/'));
+
+			handleNode(scene->mRootNode, scene);
+
+			return true;
+		}
+
+		void handleNode(aiNode* node, const aiScene* scene)
+		{
+			for (unsigned int i = 0; i < node->mNumMeshes; i++)
+			{
+				aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+				meshes.push_back(handleMesh(mesh, scene));
+			}
+
+			for (unsigned int i = 0; i < node->mNumChildren; i++)
+			{
+				handleNode(node->mChildren[i], scene);
+			}
+		}
+
+		Mesh handleMesh(aiMesh* mesh, const aiScene* scene)
+		{
+			std::vector<Vertex> vertices;
+			std::vector<GLuint> indices;
+			std::vector<Texture*> textures;
+
+			for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+			{
+				Vertex vertex;
+
+				if (mesh->HasPositions())
 				{
-					indices.push_back(face.mIndices[j]);
+					vertex.Position = glm::vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z);
+				}
+				else
+				{
+					vertex.Position = glm::vec3(0.0f);
+				}
+
+				if (mesh->HasNormals())
+				{
+					vertex.Normal = glm::vec3(mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z);
+				}
+				else
+				{
+					vertex.Normal = glm::vec3(0.0f);
+				}
+
+				if (mesh->HasTextureCoords(0))
+				{
+					vertex.TexCoords = glm::vec2(mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y);
+				}
+				else
+				{
+					vertex.TexCoords = glm::vec2(0.0f);
+				}
+
+				if (mesh->HasTangentsAndBitangents())
+				{
+					vertex.Tangent = glm::vec3(mesh->mTangents[i].x, mesh->mTangents[i].y, mesh->mTangents[i].z);
+					vertex.Bitangent = glm::vec3(mesh->mBitangents[i].x, mesh->mBitangents[i].y, mesh->mBitangents[i].z);
+				}
+				else
+				{
+					vertex.Tangent = glm::vec3(0.0f);
+					vertex.Bitangent = glm::vec3(0.0f);
+				}
+
+				vertices.push_back(vertex);
+			}
+
+			if (mesh->HasFaces())
+			{
+				for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+				{
+					aiFace face = mesh->mFaces[i];
+					for (unsigned int j = 0; j < face.mNumIndices; j++)
+					{
+						indices.push_back(face.mIndices[j]);
+					}
 				}
 			}
+
+			aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+
+			std::vector<Texture*> diffuseTextures = loadMaterialTextures(material, aiTextureType_DIFFUSE, Texture::Type::Diffuse);
+			textures.insert(textures.end(), diffuseTextures.begin(), diffuseTextures.end());
+
+			std::vector<Texture*> specularTextures = loadMaterialTextures(material, aiTextureType_SPECULAR, Texture::Type::Specular);
+			textures.insert(textures.end(), specularTextures.begin(), specularTextures.end());
+
+			std::vector<Texture*> normalTextures = loadMaterialTextures(material, aiTextureType_NORMALS, Texture::Type::Normal);
+			textures.insert(textures.end(), normalTextures.begin(), normalTextures.end());
+
+			std::vector<Texture*> heightTextures = loadMaterialTextures(material, aiTextureType_HEIGHT, Texture::Type::Height);
+			textures.insert(textures.end(), heightTextures.begin(), heightTextures.end());
+
+			return *Mesh::createWithData(vertices, indices, Mesh::DrawType::Elements);
 		}
-		
-		aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-		
-		std::vector<Texture*> diffuseTextures = loadMaterialTextures(material, aiTextureType_DIFFUSE, Texture::Type::Diffuse);
-		textures.insert(textures.end(), diffuseTextures.begin(), diffuseTextures.end());
-		
-		std::vector<Texture*> specularTextures = loadMaterialTextures(material, aiTextureType_SPECULAR, Texture::Type::Specular);
-		textures.insert(textures.end(), specularTextures.begin(), specularTextures.end());
 
-		std::vector<Texture*> normalTextures = loadMaterialTextures(material, aiTextureType_NORMALS, Texture::Type::Normal);
-		textures.insert(textures.end(), normalTextures.begin(), normalTextures.end());
-
-		std::vector<Texture*> heightTextures = loadMaterialTextures(material, aiTextureType_HEIGHT, Texture::Type::Height);
-		textures.insert(textures.end(), heightTextures.begin(), heightTextures.end());
-		
-		return *Mesh::createWithData(vertices, indices, Mesh::DrawType::Elements);
-	}
-
-	std::vector<Texture*> loadMaterialTextures(aiMaterial* mat, aiTextureType type, Texture::Type texType)
-	{
-		std::vector<Texture*> textures;
-		for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+		std::vector<Texture*> loadMaterialTextures(aiMaterial* mat, aiTextureType type, Texture::Type texType)
 		{
-			aiString str;
-			mat->GetTexture(type, i, &str);
-			
-			std::string texName = std::string(str.C_Str());
-			if (texName.find('/') != texName.npos)
+			std::vector<Texture*> textures;
+			for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
 			{
-				texName = texName.substr(texName.find_last_of('/') + 1);
-			}
-			else if (texName.find('\\') != texName.npos)
-			{
-				texName = texName.substr(texName.find_last_of('\\') + 1);
-			}
+				aiString str;
+				mat->GetTexture(type, i, &str);
 
-			std::string texPath = directory + '/' + texName;
-
-			bool alreadyLoaded = false;
-			for (unsigned int j = 0; j < texturesLoaded.size(); j++)
-			{
-				if (std::strcmp(texturesLoaded[j]->getFilePath().data(), str.C_Str()) == 0)
+				std::string texName = std::string(str.C_Str());
+				if (texName.find('/') != texName.npos)
 				{
-					textures.push_back(texturesLoaded[j]);
-					alreadyLoaded = true;
-					break;
+					texName = texName.substr(texName.find_last_of('/') + 1);
+				}
+				else if (texName.find('\\') != texName.npos)
+				{
+					texName = texName.substr(texName.find_last_of('\\') + 1);
+				}
+
+				std::string texPath = directory + '/' + texName;
+
+				bool alreadyLoaded = false;
+				for (unsigned int j = 0; j < texturesLoaded.size(); j++)
+				{
+					if (std::strcmp(texturesLoaded[j]->getFilePath().data(), str.C_Str()) == 0)
+					{
+						textures.push_back(texturesLoaded[j]);
+						alreadyLoaded = true;
+						break;
+					}
+				}
+				if (!alreadyLoaded)
+				{
+					Texture* tex = Texture::createWithFile(texPath, texType);
+					textures.push_back(tex);
+					texturesLoaded.push_back(tex);
 				}
 			}
-			if (!alreadyLoaded)
-			{
-				Texture* tex = Texture::createWithFile(texPath, texType);
-				textures.push_back(tex);
-				texturesLoaded.push_back(tex);
-			}
+			return textures;
 		}
-		return textures;
-	}
-};
+	};
+}
 
--- src/10.model_loading/model_loading.cpp
+++ src/10.model_loading/model_loading.cpp
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 #define GLFW_INCLUDE_ES32
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -148,4 +148,151 @@ void mouseCallback(GLFWwindow* window, double xPos, double yPos)
 void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
 {
 	camera.handleMouseScroll(yOffset);
+}
+*/
+
+
+#include <common.h>
+using namespace es;
+
+class Example final : public ExampleBase
+{
+public:
+	Mesh* cube;
+	Material* material;
+	Example()
+	{
+		title = "model loading";
+		defaultClearColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+		modelsDirectory = getResourcesPath(ResourceType::Model) + "/10.model_loading/";
+		shadersDirectory = getResourcesPath(ResourceType::Shader) + "/10.model_loading/";
+		texturesDirectory = getResourcesPath(ResourceType::Texture) + "/10.model_loading/";
+	}
+	~Example()
+	{
+		delete(cube);
+	}
+public:
+	virtual void prepare() override
+	{
+		// setup camera
+		camera.type = Camera::Type::firstPerson;
+		camera.rotationSpeed = 0.5f;
+		camera.setPosition(glm::vec3(0.0f, 0.0f, -4.0f));
+
+		glEnable(GL_DEPTH_TEST);
+
+		std::vector<GLfloat> vertexAttrs = {
+			// positions         // texture coordinates
+		   -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		   -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		   -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		   -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		};
+
+		std::vector<Vertex> vertices = {};
+		for (uint32_t i = 0; i < static_cast<uint32_t>(vertexAttrs.size() / 5); i++)
+		{
+			Vertex vertex;
+			vertex.Position = glm::vec3(vertexAttrs[i * 5], vertexAttrs[i * 5 + 1], vertexAttrs[i * 5 + 2]);
+			vertex.TexCoords = glm::vec2(vertexAttrs[i * 5 + 3], vertexAttrs[i * 5 + 4]);
+			vertices.push_back(vertex);
+		}
+
+		std::unordered_map<Material::ShaderType, std::string> shaderPaths =
+		{
+			{ Material::ShaderType::Vertex, shadersDirectory + "model.vert" },
+			{ Material::ShaderType::Fragment, shadersDirectory + "model.frag" }
+		};
+
+		std::vector<std::pair<Texture::Type, std::string>> texturePaths =
+		{
+			std::make_pair(Texture::Type::Diffuse, texturesDirectory + "face.png")
+		};
+
+		// create quad material
+		material = Material::createWithFile(shaderPaths, texturePaths);
+
+		cube = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, material);
+	}
+	virtual void render() override
+	{
+		if (!paused || camera.updated)
+		{
+			// initialize matrix to identity matrix
+			glm::mat4 model = glm::mat4(1.0f);
+
+			// translation
+			model = glm::translate(model, glm::vec3(0.1f, 0.4f, 0.3f));
+
+			// rotation
+			model = glm::rotate(model, timePassed * 2.0f * glm::radians(90.0f), glm::vec3(0.5f, 0.2f, 0.8f));
+
+			// scale
+			float scale = glm::max(glm::sin((double)timePassed * 2.0), 0.3);
+			model = glm::scale(model, glm::vec3(scale));
+
+			material->setMat4("model", model);
+			material->setMat4("view", camera.matrices.view);
+			material->setMat4("projection", camera.matrices.projection);
+		}
+
+		// render quad mesh
+		cube->Draw();
+	}
+};
+
+Example* example;
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
+{
+	example = new Example();
+	example->setupValidation();
+	if (!example->setupGLFW() ||
+		!example->loadGLESFunctions() ||
+		!example->setupImGui())
+	{
+		return 0;
+	}
+	example->prepare();
+	example->renderLoop();
+	delete(example);
+	return 0;
 }
