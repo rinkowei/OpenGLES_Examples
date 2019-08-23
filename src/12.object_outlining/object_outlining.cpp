6fb01ca6d318d3c328cc205b54b71fb4a97f9853
Rinkowei <282633894@qq.com>
2019-08-24 00:35:06 +0800

    updated 12.object_outlining project

--- src/12.object_outlining/object_outlining.cpp
+++ src/12.object_outlining/object_outlining.cpp
@@ -265,8 +265,11 @@ using namespace es;
 class Example final : public ExampleBase
 {
 public:
-	Mesh* cube;
-	Mesh* outlineCube;
+	Mesh* cube1;
+	Mesh* cube2;
+
+	Mesh* outlineCube1;
+	Mesh* outlineCube2;
 
 	Example()
 	{
@@ -279,8 +282,10 @@ public:
 	}
 	~Example()
 	{
-		delete(cube);
-		delete(outlineCube);
+		delete(cube1);
+		delete(cube2);
+		delete(outlineCube1);
+		delete(outlineCube2);
 	}
 public:
 	virtual void prepare() override
@@ -288,13 +293,14 @@ public:
 		ExampleBase::prepare();
 
 		// setup camera
-		camera->rotationSpeed = 0.5f;
 		camera->movementSpeed = 1.0f;
-		camera->setPosition(glm::vec3(0.0f, 0.0f, -4.0f));
+		camera->rotationSpeed = 0.5f;
+		camera->setPosition(glm::vec3(0.0f, 0.0f, 3.0f));
 
 		// enable depth test
 		glEnable(GL_DEPTH_TEST);
 
+		// enable stencil test
 		glEnable(GL_STENCIL_TEST);
 		glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
 		glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
@@ -376,26 +382,44 @@ public:
 		};
 
 		// create cube material
-		Material* cubeMat = Material::createWithFile(cubeShaderPaths, cubeTexturePaths);
-		Material* outlineMat = Material::createWithFile(outlineShaderPaths, outlineTexturePaths);
+		std::shared_ptr<Material> cubeMat = std::make_shared<Material>(cubeShaderPaths, cubeTexturePaths);
+		// create outline material
+		std::shared_ptr<Material> outlineMat = std::make_shared<Material>(outlineShaderPaths, outlineTexturePaths);
 
-		cube = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, cubeMat);
-		outlineCube = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, outlineMat);
-		outlineCube->setScale(glm::vec3(1.2f, 1.2f, 1.2f));
+		// create cube1 mesh
+		cube1 = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, cubeMat);
+		// create outline1 mesh
+		outlineCube1 = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, outlineMat);
+		outlineCube1->setScale(glm::vec3(1.1f, 1.1f, 1.1f));
+
+		// create cube2 mesh
+		cube2 = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, cubeMat);
+		cube2->setPosition(glm::vec3(1.0f, 0.0f, -2.0f));
+		// create outline2 mesh
+		outlineCube2 = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, outlineMat);
+		outlineCube2->setPosition(glm::vec3(1.0f, 0.0f, -2.0f));
+		outlineCube2->setScale(glm::vec3(1.1f, 1.1f, 1.1f));
 	}
 
-	virtual void update() override
+	virtual void render(float deltaTime) override
 	{
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+		glClearColor(defaultClearColor.r, defaultClearColor.g, defaultClearColor.b, defaultClearColor.a);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
 		glStencilFunc(GL_ALWAYS, 1, 0xFF);
 		glStencilMask(0xFF);
 
-		cube->render();
+		cube1->render(deltaTime);
+		cube2->render(deltaTime);
 
 		glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
 		glStencilMask(0x00);
 		glDisable(GL_DEPTH_TEST);
 
-		outlineCube->render();
+		outlineCube1->render(deltaTime);
+		outlineCube2->render(deltaTime);
 
 		glStencilMask(0xFF);
 		glEnable(GL_DEPTH_TEST);
