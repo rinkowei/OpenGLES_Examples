958b29aa49d8236b1420da47c01188a46a3f3eec
linziwei <linziwei@kingsoft.com>
2019-08-26 15:52:21 +0800

    updated 18.uniform_buffer_object project

--- resources/shaders/18.uniform_buffer_object/red.frag
+++ resources/shaders/18.uniform_buffer_object/red.frag
@@ -2,11 +2,7 @@
 precision mediump float;
 layout(location = 0) out vec4 fragColor;
 
-in vec2 fTexCoord;
-
-uniform sampler2D diffuseMap_0;
-
 void main()
 {
-    fragColor = texture(diffuseMap_0, fTexCoord);
+    fragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
--- src/18.uniform_buffer_object/uniform_buffer_object.cpp
+++ src/18.uniform_buffer_object/uniform_buffer_object.cpp
@@ -5,24 +5,25 @@ using namespace es;
 class Example final : public ExampleBase
 {
 public:
-	Model* model;
-	Mesh* offscreenQuad;
-	GLuint framebuffer, colorAttachment, renderbuffer;
+	Mesh* cubeBlue;
+	Mesh* cubeGreen;
+	Mesh* cubeRed;
+	Mesh* cubeYellow;
 
 	Example()
 	{
 		title = "uniform buffer object";
 		settings.vsync = true;
-		defaultClearColor = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+		defaultClearColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 
-		modelsDirectory = getResourcesPath(ResourceType::Model);
 		shadersDirectory = getResourcesPath(ResourceType::Shader) + "/18.uniform_buffer_object/";
 	}
 	~Example()
 	{
-		delete(model);
-		delete(offscreenQuad);
-		glDeleteFramebuffers(1, &framebuffer);
+		delete(cubeBlue);
+		delete(cubeGreen);
+		delete(cubeRed);
+		delete(cubeYellow);
 	}
 public:
 	virtual void prepare() override
@@ -30,76 +31,122 @@ public:
 		ExampleBase::prepare();
 
 		// setup camera
-		camera->movementSpeed = 20.0f;
-		camera->rotationSpeed = 0.5f;
-		camera->setPosition(glm::vec3(0.0f, 0.0f, 4.0f));
-		//camera->setRotation(glm::vec3(0.0f, 0.0f, 0.0f));
+		camera->movementSpeed = 2.0f;
+		camera->rotationSpeed = 1.0f;
+		camera->setPosition(glm::vec3(0.0f, 0.0f, 3.0f));
 
 		// enable depth test
 		glEnable(GL_DEPTH_TEST);
 
 		std::vector<GLfloat> vertexAttrs = {
-			 // positions       // texture coordinates
-			 0.4f, 1.0f, 0.0f,  1.0f, 1.0f,
-			 0.4f, 0.2f, 0.0f,  1.0f, 0.0f,
-			-0.4f, 0.2f, 0.0f,  0.0f, 0.0f,
-			-0.4f, 1.0f, 0.0f,  0.0f, 1.0f
-		};
+			// positions         
+			-0.5f, -0.5f, -0.5f,
+			 0.5f, -0.5f, -0.5f,
+			 0.5f,  0.5f, -0.5f,
+			 0.5f,  0.5f, -0.5f,
+			-0.5f,  0.5f, -0.5f,
+			-0.5f, -0.5f, -0.5f,
 
-		std::vector<GLuint> indices = {
-			0, 1, 3,
-			1, 2, 3
+			-0.5f, -0.5f,  0.5f,
+			 0.5f, -0.5f,  0.5f,
+			 0.5f,  0.5f,  0.5f,
+			 0.5f,  0.5f,  0.5f,
+			-0.5f,  0.5f,  0.5f,
+			-0.5f, -0.5f,  0.5f,
+
+			-0.5f,  0.5f,  0.5f,
+			-0.5f,  0.5f, -0.5f,
+			-0.5f, -0.5f, -0.5f,
+			-0.5f, -0.5f, -0.5f,
+			-0.5f, -0.5f,  0.5f,
+			-0.5f,  0.5f,  0.5f,
+
+			 0.5f,  0.5f,  0.5f,
+			 0.5f,  0.5f, -0.5f,
+			 0.5f, -0.5f, -0.5f,
+			 0.5f, -0.5f, -0.5f,
+			 0.5f, -0.5f,  0.5f,
+			 0.5f,  0.5f,  0.5f,
+
+			-0.5f, -0.5f, -0.5f,
+			 0.5f, -0.5f, -0.5f,
+			 0.5f, -0.5f,  0.5f,
+			 0.5f, -0.5f,  0.5f,
+			-0.5f, -0.5f,  0.5f,
+			-0.5f, -0.5f, -0.5f,
+
+			-0.5f,  0.5f, -0.5f,
+			 0.5f,  0.5f, -0.5f,
+			 0.5f,  0.5f,  0.5f,
+			 0.5f,  0.5f,  0.5f,
+			-0.5f,  0.5f,  0.5f,
+			-0.5f,  0.5f, -0.5f
 		};
 
 		std::vector<Vertex> vertices = {};
-		for (uint32_t i = 0; i < static_cast<uint32_t>(vertexAttrs.size() / 5); i++)
+		for (uint32_t i = 0; i < static_cast<uint32_t>(vertexAttrs.size() / 3); i++)
 		{
 			Vertex vertex;
-			vertex.Position = glm::vec3(vertexAttrs[i * 5], vertexAttrs[i * 5 + 1], vertexAttrs[i * 5 + 2]);
-			vertex.TexCoords = glm::vec2(vertexAttrs[i * 5 + 3], vertexAttrs[i * 5 + 4]);
+			vertex.Position = glm::vec3(vertexAttrs[i * 3], vertexAttrs[i * 3 + 1], vertexAttrs[i * 3 + 2]);
 			vertices.push_back(vertex);
 		}
 
-		std::unordered_map<Material::ShaderType, std::string> shaderPaths =
+		std::unordered_map<Material::ShaderType, std::string> blueShaderPaths =
 		{
-			{ Material::ShaderType::Vertex, shadersDirectory + "screen.vert" },
-			{ Material::ShaderType::Fragment, shadersDirectory + "screen.frag" }
+			{ Material::ShaderType::Vertex, shadersDirectory + "ubo.vert" },
+			{ Material::ShaderType::Fragment, shadersDirectory + "blue.frag" }
+		};
+
+		std::unordered_map<Material::ShaderType, std::string> greenShaderPaths =
+		{
+			{ Material::ShaderType::Vertex, shadersDirectory + "ubo.vert" },
+			{ Material::ShaderType::Fragment, shadersDirectory + "green.frag" }
+		};
+
+		std::unordered_map<Material::ShaderType, std::string> redShaderPaths =
+		{
+			{ Material::ShaderType::Vertex, shadersDirectory + "ubo.vert" },
+			{ Material::ShaderType::Fragment, shadersDirectory + "red.frag" }
+		};
+
+		std::unordered_map<Material::ShaderType, std::string> yellowShaderPaths =
+		{
+			{ Material::ShaderType::Vertex, shadersDirectory + "ubo.vert" },
+			{ Material::ShaderType::Fragment, shadersDirectory + "yellow.frag" }
 		};
 
 		std::vector<std::pair<Texture::Type, std::string>> texturePaths =
 		{
-
+			
 		};
 
-		// create offscreenQuad material
-		std::shared_ptr<Material> material = std::make_shared<Material>(shaderPaths, texturePaths);
+		// create cubeBlue material
+		std::shared_ptr<Material> blueMaterial = std::make_shared<Material>(blueShaderPaths, texturePaths);
 
-		// create offscreenQuad mesh
-		offscreenQuad = Mesh::createWithData(vertices, indices, Mesh::DrawType::Elements, material);
-		
-		model = Model::createWithFile(modelsDirectory + "/construction-site-rawscan/site.obj", 
-		{
-			{ Material::ShaderType::Vertex, shadersDirectory + "construction.vert" },
-			{ Material::ShaderType::Fragment, shadersDirectory + "construction.frag" }
-		});
+		// create cubeGreen material
+		std::shared_ptr<Material> greenMaterial = std::make_shared<Material>(greenShaderPaths, texturePaths);
 
-		// configure framebuffer
-		glGenFramebuffers(1, &framebuffer);
-		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		// create cubeRed material
+		std::shared_ptr<Material> redMaterial = std::make_shared<Material>(redShaderPaths, texturePaths);
 
-		// attach one color buffer for write pixels data
-		glGenTextures(1, &colorAttachment);
-		glBindTexture(GL_TEXTURE_2D, colorAttachment);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorAttachment, 0);
+		// create cubeYellow material
+		std::shared_ptr<Material> yellowMaterial = std::make_shared<Material>(yellowShaderPaths, texturePaths);
 
-		// attach renderbuffer for write depth value and stencil valie
-		glGenRenderbuffers(1, &renderbuffer);
-		glBindRenderbuffer(GL_RENDERBUFFER, renderbuffer);
-		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
-		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderbuffer);
+		// create cubeBlue mesh
+		cubeBlue = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, blueMaterial);
+		cubeBlue->setPosition(glm::vec3(-0.75f, 0.75f, 0.0f));
+
+		// create cubeGreen mesh
+		cubeGreen = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, greenMaterial);
+		cubeGreen->setPosition(glm::vec3(0.75f, 0.75f, 0.0f));
+
+		// create cubeRed mesh
+		cubeRed = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, redMaterial);
+		cubeRed->setPosition(glm::vec3(-0.75f, -0.75f, 0.0f));
+
+		// create cubeYellow mesh
+		cubeYellow = Mesh::createWithData(vertices, {}, Mesh::DrawType::Arrays, yellowMaterial);
+		cubeYellow->setPosition(glm::vec3(0.75f, -0.75f, 0.0f));
 	}
 
 	virtual void render(float deltaTime) override
@@ -107,26 +154,13 @@ public:
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
 		glClearColor(defaultClearColor.r, defaultClearColor.g, defaultClearColor.b, defaultClearColor.a);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-		// clear color and depth buffer
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glEnable(GL_DEPTH_TEST);
-		model->render(deltaTime);
-
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-		model->render(deltaTime);
 
-		// diable depth test for render quad in front of scene
-		glDisable(GL_DEPTH_TEST);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, colorAttachment);
-		offscreenQuad->getMaterial()->setInt("screenTexture", 0);
-		offscreenQuad->render(deltaTime);
+		
+		cubeBlue->render(deltaTime);
+		cubeGreen->render(deltaTime);
+		cubeRed->render(deltaTime);
+		cubeYellow->render(deltaTime);
 	}
 };
 
