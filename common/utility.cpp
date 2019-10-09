7ced2ec4c38733bd57f4a0e400e26b86467b5f05
Rinkowei <282633894@qq.com>
2019-10-09 23:55:07 +0800

    updated Utility class

--- common/utility.cpp
+++ common/utility.cpp
@@ -1,7 +1,62 @@
-
 #include "utility.h"
+#include "ogles.h"
+
+#ifdef WIN32
+#include <Windows.h>
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#define ChangeWorkingDir _chdir
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#define ChangeWorkingDir chdir
+#endif
+
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
 
 namespace es
 {
+	std::string Utility::execPath = "";
 
+	std::string Utility::pathForResource(const std::string& resource)
+	{
+
+	}
+
+	std::string Utility::executablePath()
+	{
+#ifdef WIN32
+		if (execPath == "")
+		{
+			char buffer[1024];
+			GetModuleFileName(nullptr, &buffer[0], 1024);
+			execPath = buffer;
+			execPath = pathWithoutFile(execPath);
+		}
+#elif __APPLE__
+
+#else
+
+#endif
+
+		return execPath;
+	}
+
+	std::string Utility::pathWithoutFile(const std::string& path)
+	{
+#ifdef WIN32
+		std::replace(path.begin(), path.end(), '\\', '/');
+#endif
+		std::size_t found = path.find_last_of("/\\");
+		return path.substr(0, found);
+	}
+
+	std::string Utility::fileExtension(const std::string& filePath)
+	{
+		std::size_t found = filePath.find_last_of(".");
+		std::string ext = filePath.substr(found, filePath.size());
+		return ext;
+	}
 }
--- common/utility.h
+++ common/utility.h
@@ -1,12 +1,30 @@
 #pragma once
 
-#include <iostream>
-#include <memory>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <algorithm>
+#include <stdio.h>
 
 namespace es
 {
 	class Utility
 	{
+	public:
+		static std::string pathForResource(const std::string& resource);
 
+		static std::string executablePath();
+
+		//static bool readFile(const std::string& path, std::string& out);
+
+		//static bool preprocessShader(const std::string& path, const std::string& src, std::string& out);
+
+		static std::string pathWithoutFile(const std::string& path);
+
+		static std::string fileExtension(const std::string& filePath);
+
+		static std::string execPath;
 	};
 }
