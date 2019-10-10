09cc3414e749df4b7934abc6b2469b3368256704
Rinkowei <282633894@qq.com>
2019-10-10 22:46:38 +0800

    updated Buffer class

--- common/buffer.cpp
+++ common/buffer.cpp
@@ -3,8 +3,60 @@
 
 namespace es
 {
-	Buffer::Buffer(GLenum type, GLenum usage, size_t size, void* data)
+	Buffer::Buffer(GLenum type, GLenum usage, std::size_t size, void* data)
+		:mType(type),
+		 mSize(size)
 	{
+		GLES_CHECK_ERROR(glGenBuffers(1, &mID));
+		GLES_CHECK_ERROR(glBindBuffer(type, mID));
+		GLES_CHECK_ERROR(glBufferData(type, size, data, usage));
+		GLES_CHECK_ERROR(glBindBuffer(type, 0));
+	}
 
+	Buffer::~Buffer()
+	{
+		GLES_CHECK_ERROR(glDeleteBuffers(1, &mID));
+	}
+
+	void Buffer::bind()
+	{
+		GLES_CHECK_ERROR(glBindBuffer(mType, mID));
+	}
+
+	void Buffer::bindBase(int index)
+	{
+		GLES_CHECK_ERROR(glBindBufferBase(mType, index, mID));
+	}
+
+	void Buffer::bindRange(int index, std::size_t offset, std::size_t size)
+	{
+		GLES_CHECK_ERROR(glBindBufferRange(mType, index, mID, offset, size));
+	}
+
+	void Buffer::unbind()
+	{
+		GLES_CHECK_ERROR(glBindBuffer(mType, 0));
+	}
+
+	void* Buffer::mapRange(GLenum access, std::size_t offset, std::size_t size)
+	{
+		GLES_CHECK_ERROR(glBindBuffer(mType, mID));
+		GLES_CHECK_ERROR(void* ptr = glMapBufferRange(mType, offset, size, access));
+		GLES_CHECK_ERROR(glBindBuffer(mType, 0));
+		return ptr;
+	}
+
+	void Buffer::unMap()
+	{
+		GLES_CHECK_ERROR(glBindBuffer(mType, mID));
+		GLES_CHECK_ERROR(glUnmapBuffer(mType));
+		GLES_CHECK_ERROR(glBindBuffer(mType, 0));
+	}
+
+	void Buffer::setData(std::size_t offset, std::size_t size, void* data)
+	{
+		GLES_CHECK_ERROR(glBindBuffer(mType, mID));
+		GLES_CHECK_ERROR(glBufferSubData(mType, offset, size, data));
+		GLES_CHECK_ERROR(glBindBuffer(mType, 0));
 	}
 }
--- common/buffer.h
+++ common/buffer.h
@@ -7,22 +7,21 @@ namespace es
 	class Buffer
 	{
 	public:
-		Buffer(GLenum type, GLenum usage, size_t size, void* data);
+		Buffer(GLenum type, GLenum usage, std::size_t size, void* data);
 		virtual ~Buffer();
 
 		void bind();
 		void bindBase(int index);
-		void bindRange(int index, size_t offset, size_t size);
+		void bindRange(int index, std::size_t offset, std::size_t size);
 		void unbind();
 
-		void* map(GLenum access);
-		void* mapRange(GLenum access, size_t offset, size_t size);
+		void* mapRange(GLenum access, std::size_t offset, std::size_t size);
 		void unMap();
 
-		void setData(size_t offset, size_t size, void* data);
+		void setData(std::size_t offset, std::size_t size, void* data);
 	protected:
 		GLenum mType;
-		GLuint mGLBuffer;
+		GLuint mID;
 		size_t mSize;
 	};
 }
