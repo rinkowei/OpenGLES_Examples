faaea62c1847003b22ca7bbbcd3822ab8cd34bd5
Rinkowei <282633894@qq.com>
2019-10-14 21:41:15 +0800

    updated 04.quadrangle project

--- src/04.quadrangle_indices/quadrangle_indices.cpp
+++ src/04.quadrangle_indices/quadrangle_indices.cpp
@@ -1,11 +1,13 @@
-﻿
-#include <common.h>
+﻿#include <examplebase.h>
+#include <mesh.h>
+#include <material.h>
 using namespace es;
 
 class Example final : public ExampleBase
 {
 public:
-	Mesh* quadrangle;
+	std::shared_ptr<Mesh> quadrangle;
+	std::unique_ptr<Program> program;
 
 	Example()
 	{
@@ -17,14 +19,15 @@ public:
 	}
 	~Example()
 	{
-		delete(quadrangle);
+	
 	}
 public:
 	virtual void prepare() override
 	{
 		ExampleBase::prepare();
 
-		std::vector<GLfloat> vertexPositions = {
+		std::vector<float> vertexAttribs = {
+			 // position
 			 0.5f,  0.5f, 0.0f,
 			 0.5f, -0.5f, 0.0f,
 			-0.5f, -0.5f, 0.0f,
@@ -37,26 +40,23 @@ public:
 		};
 
 		std::vector<Vertex> vertices = {};
-		for (uint32_t i = 0; i < static_cast<uint32_t>(vertexPositions.size() / 3); i++)
+		for (uint32_t i = 0; i < static_cast<uint32_t>(vertexAttribs.size() / 3); i++)
 		{
 			Vertex vertex;
-			vertex.Position = glm::vec3(vertexPositions[i * 3], vertexPositions[i * 3 + 1], vertexPositions[i * 3 + 2]);
+			vertex.vPosition = glm::vec3(vertexAttribs[i * 3], vertexAttribs[i * 3 + 1], vertexAttribs[i * 3 + 2]);
 			vertices.push_back(vertex);
 		}
 
-		std::unordered_map<Material::ShaderType, std::string> shaderPaths =
-		{
-			{ Material::ShaderType::VERTEX, shadersDirectory + "quadrangle.vert" },
-			{ Material::ShaderType::FRAGMENT, shadersDirectory + "quadrangle.frag" }
-		};
-
-		std::vector<std::pair<Texture::Type, std::string>> texturePaths = {};
-
-		// create quadrangle material
-		std::shared_ptr<Material> material = std::make_shared<Material>(shaderPaths, texturePaths);
-
 		// create quadrangle mesh
-		quadrangle = Mesh::createWithData(vertices, indices, material, Mesh::DrawType::ELEMENTS);
+		quadrangle = Mesh::createWithData("quadrangle", vertices, indices);
+		quadrangle->setDrawType(Mesh::DrawType::ELEMENTS);
+
+		program = Program::createFromFiles(
+			{
+				shadersDirectory + "quadrangle.vert",
+				shadersDirectory + "quadrangle.frag"
+			}
+		);
 	}
 
 	virtual void render(float deltaTime) override
@@ -66,7 +66,8 @@ public:
 		glClearColor(defaultClearColor.r, defaultClearColor.g, defaultClearColor.b, defaultClearColor.a);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		quadrangle->render(deltaTime);
+		program->apply();
+		quadrangle->render();
 	}
 };
 
