49bf1f4ba90180a38ad511978b2a2992965a0021
linziwei <282633894@qq.com>
2019-10-29 03:56:23 +0800

    updated TextureCube class

--- common/texture.cpp
+++ common/texture.cpp
@@ -603,43 +603,4 @@ namespace es
 	{
 		return mMipLevels;
 	}
-
-	/*
-
-	GLboolean TextureCube::initWithFiles(const std::vector<std::string>& filePaths)
-	{
-		this->filePaths = filePaths;
-
-		glGenTextures(1, &ID);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, ID);
-
-		int width, height, nrChannels;
-		for (size_t i = 0; i < filePaths.size(); i++)
-		{
-			unsigned char* data = stbi_load(filePaths[i].c_str(), &width, &height, &nrChannels, 0);
-			if (data)
-			{
-				if (nrChannels == 1)
-					glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data);
-				else if (nrChannels == 3)
-					glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-				else if (nrChannels == 4)
-					glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-				stbi_image_free(data);
-			}
-			else
-			{
-				std::cout << "failed to create cubemap texture at path : " << filePaths[i] << std::endl;
-				stbi_image_free(data);
-			}
-		}
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-		return true;
-	}
-	*/
 }
