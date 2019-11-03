9b8f949fc228aa0bb5c09e9ca250181cf5a51e70
linziwei <282633894@qq.com>
2019-11-03 21:52:28 +0800

    optimize code

--- src/19.skybox_reflect/skybox_reflect.cpp
+++ src/19.skybox_reflect/skybox_reflect.cpp
@@ -13,7 +13,6 @@ public:
 	{
 		title = "skybox reflect";
 		settings.vsync = true;
-		settings.validation = true;
 		defaultClearColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 
 		modelsDirectory = getResourcesPath(ResourceType::Model);
@@ -56,8 +55,8 @@ public:
 
 	virtual void render(float deltaTime) override
 	{
-		SDL_GetWindowSize(window, &destWidth, &destHeight);
-		glViewport(0, 0, destWidth, destHeight);
+		SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+		glViewport(0, 0, windowWidth, windowHeight);
 		glClearColor(defaultClearColor.r, defaultClearColor.g, defaultClearColor.b, defaultClearColor.a);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 	
