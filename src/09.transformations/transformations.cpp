c4d8640cd2167334858b9d8ec6fd57a0f13f7041
linziwei <282633894@qq.com>
2019-11-03 21:44:21 +0800

    optimize code

--- src/09.transformations/transformations.cpp
+++ src/09.transformations/transformations.cpp
@@ -102,8 +102,8 @@ public:
 
 	virtual void render(float deltaTime) override
 	{
-		SDL_GetWindowSize(window, &destWidth, &destHeight);
-		glViewport(0, 0, destWidth, destHeight);
+		SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+		glViewport(0, 0, windowWidth, windowHeight);
 		glClearColor(defaultClearColor.r, defaultClearColor.g, defaultClearColor.b, defaultClearColor.a);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
