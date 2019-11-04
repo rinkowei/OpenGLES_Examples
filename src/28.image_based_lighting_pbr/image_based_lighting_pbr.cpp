90adfd17ccc85f41de44822520bf7fcd2520f13f
linziwei <282633894@qq.com>
2019-11-04 22:51:12 +0800

    updated 28.image_based_lighting project

--- src/28.image_based_lighting_pbr/image_based_lighting_pbr.cpp
+++ src/28.image_based_lighting_pbr/image_based_lighting_pbr.cpp
@@ -13,6 +13,9 @@ public:
 	const int col = 7;
 	std::vector<std::shared_ptr<Model>> spheres;
 
+	std::unique_ptr<Framebuffer> captureFBO;
+	std::unique_ptr<Renderbuffer> captureRBO;
+
 	struct Light
 	{
 		glm::vec3 color;
@@ -38,14 +41,36 @@ public:
 	virtual void prepare() override
 	{
 		ExampleBase::prepare();
-
+	
 		// setup camera
-		mMainCamera->setPosition(glm::vec3(0.0f, 10.0f, 12.0f));
-		mMainCamera->setRotation(glm::vec3(45.0f, 0.0f, 0.0f));
+		mMainCamera->setPosition(glm::vec3(0.0f, 0.0f, 15.0f));
+		mMainCamera->setRotation(glm::vec3(0.0f, 0.0f, 0.0f));
 
 		// enable depth test
 		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
 
+		captureFBO = Framebuffer::create();
+
+		captureRBO = Renderbuffer::create(GL_DEPTH_COMPONENT24, 512, 512);
+		captureFBO->attachRenderBufferTarget(captureRBO.get());
+
+		std::shared_ptr<Texture2D> hdrTexture = Texture2D::createFromFile(texturesDirectory + "/sIBL/TropicalRuins.hdr");
+
+		std::shared_ptr<TextureCube> envCubemap = TextureCube::createFromData("environment_map", 512, 512, 1, GL_RGB16F, GL_RGB, GL_FLOAT, nullptr);
+		envCubemap->setMinFilter(GL_LINEAR);
+		envCubemap->setMagFilter(GL_LINEAR);
+		envCubemap->setWrapping(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
+
+		glm::mat4 captureProjection = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 10.0f);
+		std::array<glm::mat4, 6> captureViews = {
+			glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+			glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+			glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  1.0f,  0.0f), glm::vec3(0.0f,  0.0f,  1.0f)),
+			glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f,  0.0f), glm::vec3(0.0f,  0.0f, -1.0f)),
+			glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  0.0f,  1.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+			glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f))
+		};
 		std::shared_ptr<Model> sphereTemplate = Model::createFromFile("sphere_template", modelsDirectory + "/sphere/sphere.obj",
 			{
 				shadersDirectory + "pbr.vert",
@@ -65,9 +90,9 @@ public:
 			{
 				std::shared_ptr<Model> sphere = Model::clone("sphere_" + std::to_string(x * col + y), sphereTemplate.get());
 			
-				glm::vec3 pos = glm::vec3(float(y - (row / 2.0f)) * 2.5f, 0.0f, float(x - (col / 2.0f)) * 2.5f);
+				glm::vec3 pos = glm::vec3(float(y - (row / 2.0f)) * 2.5f, float(x - (col / 2.0f)) * 2.5f, 0.0f);
 				sphere->setPosition(pos);
-				sphere->setScale(glm::vec3(0.035f));
+				sphere->setScale(glm::vec3(0.04f));
 				sphere->setUniform("albedo", glm::vec3(1.0f, 0.765557f, 0.336057f));
 				sphere->setUniform("roughness", glm::clamp((float)x / (float)(row - 1), 0.05f, 1.0f));
 				sphere->setUniform("metallic", glm::clamp((float)col / (float)(y + 1), 0.1f, 1.0f));
