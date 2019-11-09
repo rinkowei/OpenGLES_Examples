863c5fdb49bcee3f8a3c7feced1eb2cf0b0620f9
linziwei <282633894@qq.com>
2019-11-10 01:20:02 +0800

    updated 27.compute_shader project

--- src/27.compute_shader/compute_shader.cpp
+++ src/27.compute_shader/compute_shader.cpp
@@ -12,13 +12,13 @@ public:
 	std::shared_ptr<Program> computeProgram;
 	std::shared_ptr<Model> inputQuad;
 	std::shared_ptr<Model> outputQuad;
-	GLuint tex;
+
 	Example()
 	{
 		title = "compute shader";
 		settings.vsync = true;
 		settings.validation = true;
-		defaultClearColor = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+		defaultClearColor = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
 
 		modelsDirectory = getResourcesPath(ResourceType::Model);
 		shadersDirectory = getResourcesPath(ResourceType::Shader) + "/27.compute_shader/";
@@ -55,30 +55,22 @@ public:
 		);
 
 
-		std::shared_ptr<Texture2D> inputImage = Texture2D::createFromFile(texturesDirectory + "opengles.jpg", 1, false);
-
-		std::shared_ptr<Texture2D> outputImage = Texture2D::createFromData(inputImage->getWidth(), inputImage->getHeight(), 1, 1, 1, GL_RGBA32F, GL_RGBA, GL_FLOAT);
-		//outputImage->bindImage(0, 0, 0, GL_WRITE_ONLY, GL_RGBA32F);
-	
-		tex = 0;
-
-		const unsigned int w = inputImage->getWidth();
-		const unsigned int h = inputImage->getHeight();
-
-		glGenTextures(1, &tex);
-		glBindTexture(GL_TEXTURE_2D, tex);
-
-		/* ComputeShader need to use immutable image space */
-		glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, w, h);
+		std::shared_ptr<Texture2D> inputImage = Texture2D::createFromFile(texturesDirectory + "vulkan.png", 1, false, false);
+		std::shared_ptr<Texture2D> intermediateImage = Texture2D::createFromData(inputImage->getWidth(), inputImage->getHeight(), 1, 1, GL_RGBA8, GL_RGBA, GL_UNSIGNED_BYTE, true);
+		std::shared_ptr<Texture2D> outputImage = Texture2D::createFromData(inputImage->getWidth(), inputImage->getHeight(), 1, 1, GL_RGBA8, GL_RGBA, GL_UNSIGNED_BYTE, true);
 		
 		computeProgram->apply();
 		inputImage->bindImage(0, 0, 0, GL_READ_ONLY, GL_RGBA8);
-		glBindImageTexture(1, tex, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA8);
-		glUniform1i(glGetUniformLocation(computeProgram->getID(), "inputImage"), 0);
-		glUniform1i(glGetUniformLocation(computeProgram->getID(), "outputImage"), 1);
+		intermediateImage->bindImage(1, 0, 0, GL_WRITE_ONLY, GL_RGBA8);
 		
-		glDispatchCompute(w, h, 1);
-		glMemoryBarrier(GL_ALL_BARRIER_BITS);
+		glDispatchCompute(1, 1024, 1);
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+		intermediateImage->bindImage(0, 0, 0, GL_READ_ONLY, GL_RGBA8);
+		outputImage->bindImage(1, 0, 0, GL_WRITE_ONLY, GL_RGBA8);
+
+		glDispatchCompute(1, 1024, 1);
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 		
 		computeProgram->unapply();
 
@@ -86,7 +78,7 @@ public:
 		inputQuad->setPosition(glm::vec3(0.0f, 1.05f, 0.0f));
 		inputQuad->setScale(glm::vec3(2.0f, 1.0f, 0.9f));
 		
-		//outputQuad->setTexture("inputImage", outputImage);
+		outputQuad->setTexture("inputImage", outputImage);
 		outputQuad->setPosition(glm::vec3(0.0f, -1.05f, 0.0f));
 		outputQuad->setScale(glm::vec3(2.0f, 1.0f, 0.9f));
 	}
@@ -94,8 +86,6 @@ public:
 	virtual void render(float deltaTime) override
 	{
 		inputQuad->render();
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, tex);
 		outputQuad->render();
 	}
 
