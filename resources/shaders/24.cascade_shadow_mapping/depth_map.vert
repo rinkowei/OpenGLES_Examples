98efd8bb887f114235168f48c756ede0635961ab
linziwei <282633894@qq.com>
2019-11-13 11:20:17 +0800

    updated 24.cascade_shadow_mapping project

--- resources/shaders/24.cascade_shadow_mapping/depth_map.vert
+++ resources/shaders/24.cascade_shadow_mapping/depth_map.vert
@@ -1,10 +1,14 @@
-#version 320 es
+#version 310 es
 layout(location = 0) in vec3 vPos;
+layout(location = 1) in vec2 vTexcoord;
+layout(location = 2) in vec3 vNormal;
+layout(location = 3) in vec3 vTangent;
+layout(location = 4) in vec3 vBitangent;
 
 uniform mat4 model;
-uniform mat4 lightSpaceMatrix;
+uniform mat4 lightMatrix;
 
 void main()
 {
-	gl_Position = lightSpaceMatrix * model * vec4(vPos, 1.0f);
+	gl_Position = lightMatrix * model * vec4(vPos, 1.0f);
 }
--- src/24.cascade_shadow_mapping/cascade_shadow_mapping.cpp
+++ src/24.cascade_shadow_mapping/cascade_shadow_mapping.cpp
@@ -3,11 +3,11 @@
 #include <material.h>
 using namespace es;
 
+#define NUM_CASCADES 3
+#define NUM_FRUSTUM_CORNERS 8
 class Example final : public ExampleBase
 {
 public:
-	const uint32_t NUM_CASCADES = 3;
-
 	std::array<std::shared_ptr<Model>, 5> venusShadows;
 	std::array<std::shared_ptr<Model>, 5> venuses;
 	std::shared_ptr<Model> plane;
@@ -18,13 +18,40 @@ public:
 
 	std::array<std::shared_ptr<Texture2D>, 3> lightMaps;
 
-	typedef struct DirectionalLight
+	struct PersProjInfo
+	{
+		float fov = 90.0f;
+		uint32_t width = 2048;
+		uint32_t height = 2048;
+		float zNear = 1.0f;
+		float zFar = 200.0f;
+	};
+
+	PersProjInfo persProjInfo;
+
+	struct OrthoProjInfo
+	{
+		float r;
+		float l;
+		float b;
+		float t;
+		float n;
+		float f;
+	};
+
+	std::array<OrthoProjInfo, NUM_CASCADES> lightOrthoProjInfo;
+
+	struct DirectionalLight
 	{
 		glm::vec3 color;
 		glm::vec3 direction;
 		float ambientIntensity;
 		float diffuseIntensity;
-	}dirLight;
+	};
+
+	DirectionalLight dirLight;
+
+	std::array<float, NUM_CASCADES + 1> cascadeEnd;
 
 	glm::mat4 biasMatrix = glm::mat4(
 		0.5f, 0.0f, 0.0f, 0.0f,
@@ -54,8 +81,8 @@ public:
 		ExampleBase::prepare();
 		
 		// setup camera
-		mMainCamera->setPosition(glm::vec3(0.0f, 10.0f, 15.0f));
-		mMainCamera->setRotation(glm::vec3(0.0f, 0.0f, 0.0f));
+		mMainCamera->setPosition(glm::vec3(0.0f, 10.0f, 25.0f));
+		mMainCamera->setRotation(glm::vec3(30.0f, 0.0f, 0.0f));
 
 		// enable depth test
 		glEnable(GL_DEPTH_TEST);
@@ -63,8 +90,24 @@ public:
 		// enable cull face
 		glEnable(GL_CULL_FACE);
 
-		lightMapFBO = Framebuffer::create();
+		persProjInfo.fov = 90.0f;
+		persProjInfo.width = 1024;
+		persProjInfo.height = 1024;
+		persProjInfo.zNear = 1.0f;
+		persProjInfo.zFar = 100.0f;
 
+		cascadeEnd[0] = persProjInfo.zNear;
+		cascadeEnd[1] = 15.0f;
+		cascadeEnd[2] = 50.0f;
+		cascadeEnd[3] = persProjInfo.zFar;
+
+		dirLight.color = glm::vec3(1.0f, 1.0f, 1.0f);
+		dirLight.ambientIntensity = 0.5f;
+		dirLight.diffuseIntensity = 0.9f;
+		dirLight.direction = glm::vec3(1.0f, -1.0f, 0.0f);
+
+		lightMapFBO = Framebuffer::create();
+	
 		for (std::size_t i = 0; i < lightMaps.size(); i++)
 		{
 			lightMaps[i] = Texture2D::createFromData(lightMapWidth, lightMapHeight, 1, 1, GL_DEPTH_COMPONENT32F, GL_DEPTH_COMPONENT, GL_FLOAT, false);
@@ -76,23 +119,27 @@ public:
 
 		lightMapFBO->attachDepthRenderTarget(lightMaps[0].get(), 0, 0);
 
-		plane = Model::createFromFile("plane", modelsDirectory + "/rocks_plane/rocks_plane.obj",
+		std::shared_ptr<Material> mat = Material::createFromData("mat",
 			{
 				shadersDirectory + "scene.vert",
 				shadersDirectory + "scene.frag"
+			},
+			{
+
 			}
 		);
-		plane->setRotation(glm::vec3(-90.0f, 0.0f, 0.0f));
-		plane->setScale(glm::vec3(1.0f, 2.0f, 1.0f));
 
+		plane = Model::createFromFile("plane", modelsDirectory + "/rocks_plane/rocks_plane.obj", {}, false);
+		plane->setMaterial(mat);
+		plane->setRotation(glm::vec3(-90.0f, 0.0f, 0.0f));
+		plane->setScale(glm::vec3(1.5f, 2.0f, 1.0f));
+
+		std::shared_ptr<Model> venusTemplate = Model::createFromFile("venus_template", modelsDirectory + "/venus/venus.fbx", {}, false);
+		
 		for (std::size_t i = 0; i < venuses.size(); i++)
 		{
-			venuses[i] = Model::createFromFile("venus_" + std::to_string(i), modelsDirectory + "/venus/venus.fbx",
-				{
-					shadersDirectory + "scene.vert",
-					shadersDirectory + "scene.frag"
-				}
-			);
+			venuses[i] = Model::clone("venus_" + std::to_string(i), venusTemplate.get());
+			venuses[i]->setMaterial(mat);
 			venuses[i]->setPosition(glm::vec3(0.0f, 0.0f, 15.0f - i * 7.0f));
 			venuses[i]->setScale(glm::vec3(0.2f, 0.2f, 0.2f));
 		}
@@ -121,6 +168,7 @@ public:
 
 	virtual void render(float deltaTime) override
 	{
+		calculateOrthoProjs();
 		/*
 		// change light position over time
 		lightPos = glm::vec3(sin(timePassed) * 2.0f, 5.0f + cos(timePassed) * 1.0f, cos(timePassed) * 1.0f);
@@ -162,6 +210,67 @@ public:
 	virtual void windowResized() override
 	{
 		ExampleBase::windowResized();
+	}
+
+	void calculateOrthoProjs()
+	{
+		glm::mat4 camView = mMainCamera->getView();
+		glm::mat4 camViewInv = glm::inverse(camView);
+
+		glm::mat4 lightView = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), dirLight.direction, glm::vec3(0.0f, 1.0f, 0.0f));
+
+		float aspectRatio = persProjInfo.width / persProjInfo.height;
+		float tanHalfHFov = glm::tan(glm::radians(persProjInfo.fov / 2.0f));
+		float tanHalfVFov = glm::tan(glm::radians(persProjInfo.fov * aspectRatio / 2.0f));
+
+		for (uint32_t i = 0; i < NUM_CASCADES; i++)
+		{
+			float xn = cascadeEnd[i] * tanHalfHFov;
+			float xf = cascadeEnd[i + 1] * tanHalfHFov;
+			float yn = cascadeEnd[i] * tanHalfVFov;
+			float yf = cascadeEnd[i + 1] * tanHalfVFov;
+
+			std::array<glm::vec4, NUM_FRUSTUM_CORNERS> frustumCorners = {
+				glm::vec4(xn, yn, cascadeEnd[i], 1.0f),
+				glm::vec4(-xn,  yn, cascadeEnd[i], 1.0),
+				glm::vec4(xn,  -yn, cascadeEnd[i], 1.0),
+				glm::vec4(-xn, -yn, cascadeEnd[i], 1.0),
+
+				glm::vec4(xf,   yf, cascadeEnd[i + 1], 1.0),
+				glm::vec4(-xf,  yf, cascadeEnd[i + 1], 1.0),
+				glm::vec4(xf,  -yf, cascadeEnd[i + 1], 1.0),
+				glm::vec4(-xf, -yf, cascadeEnd[i + 1], 1.0)
+			};
+
+			std::array<glm::vec4, NUM_FRUSTUM_CORNERS> frustumCornersL;
+
+			float minX = std::numeric_limits<float>::max();
+			float maxX = std::numeric_limits<float>::min();
+			float minY = std::numeric_limits<float>::max();
+			float maxY = std::numeric_limits<float>::min();
+			float minZ = std::numeric_limits<float>::max();
+			float maxZ = std::numeric_limits<float>::min();
+
+			for (uint32_t j = 0; j < NUM_FRUSTUM_CORNERS; j++)
+			{
+				glm::vec4 vW = camViewInv * frustumCorners[j];
+				frustumCornersL[j] = lightView * vW;
+
+				minX = glm::min(minX, frustumCornersL[j].x);
+				maxX = glm::max(maxX, frustumCornersL[j].x);
+				minY = glm::min(minY, frustumCornersL[j].y);
+				maxY = glm::max(maxY, frustumCornersL[j].y);
+				minZ = glm::min(minZ, frustumCornersL[j].z);
+				maxZ = glm::max(maxZ, frustumCornersL[j].z);
+			}
+
+			lightOrthoProjInfo[i].r = maxX;
+			lightOrthoProjInfo[i].l = minX;
+			lightOrthoProjInfo[i].b = minY;
+			lightOrthoProjInfo[i].t = maxY;
+			lightOrthoProjInfo[i].f = maxZ;
+			lightOrthoProjInfo[i].n = minZ;
+		}
 	}
 };
 
