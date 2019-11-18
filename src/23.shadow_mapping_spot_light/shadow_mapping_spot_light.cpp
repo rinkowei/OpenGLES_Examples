fd6bcadf523bcc423603361d9de9e6c4bcb28a46
linziwei <282633894@qq.com>
2019-11-18 21:05:42 +0800

    updated 23.shadow_mapping_spot_light project

--- src/23.shadow_mapping_spot_light/shadow_mapping_spot_light.cpp
+++ src/23.shadow_mapping_spot_light/shadow_mapping_spot_light.cpp
@@ -6,14 +6,18 @@ using namespace es;
 class Example final : public ExampleBase
 {
 public:
-	std::shared_ptr<Model> sampleSceneShadow;
 	std::shared_ptr<Model> sampleScene;
 
-	const uint16_t lightMapWidth = 2048;
-	const uint16_t lightMapHeight = 2048;
+	std::shared_ptr<Material> lightPassMat;
+	std::shared_ptr<Material> diffuseMat;
+
+	const uint32_t lightMapSize = 2048;
 	std::unique_ptr<Framebuffer> lightMapFBO;
+	std::shared_ptr<Texture2D> lightMap;
 
 	glm::vec3 lightPos = glm::vec3(0.0f, 5.0f, 0.0f);
+
+	glm::mat4 lightSpaceMatrix = glm::mat4(1.0f);
 
 	glm::mat4 biasMatrix = glm::mat4(
 		0.5f, 0.0f, 0.0f, 0.0f,
@@ -50,28 +54,27 @@ public:
 
 		// enable cull face
 		glEnable(GL_CULL_FACE);
-		glFrontFace(GL_CCW);
-		glCullFace(GL_BACK);
 
-		std::shared_ptr<Texture2D> lightMap = Texture2D::createFromData(lightMapWidth, lightMapHeight, 1, 1, 1, GL_DEPTH_COMPONENT32F, GL_DEPTH_COMPONENT, GL_FLOAT);
+		lightMap = Texture2D::createFromData(lightMapSize, lightMapSize, 1, 1, GL_DEPTH_COMPONENT32F, GL_DEPTH_COMPONENT, GL_FLOAT, true);
 		lightMap->setMinFilter(GL_NEAREST);
 		lightMap->setMagFilter(GL_NEAREST);
-		lightMap->setWrapping(GL_CLAMP_TO_BORDER_NV, GL_CLAMP_TO_BORDER_NV, GL_CLAMP_TO_BORDER_NV);
+		lightMap->setWrapping(GL_CLAMP_TO_BORDER_EXT, GL_CLAMP_TO_BORDER_EXT, GL_CLAMP_TO_BORDER_EXT);
 		lightMap->setBorderColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 		lightMapFBO = Framebuffer::create();
-		lightMapFBO->attachDepthRenderTarget(lightMap.get(), 0, 0);
+		lightMapFBO->addAttachmentTexture2D(GL_DEPTH_ATTACHMENT, lightMap->getTarget(), lightMap->getID(), 0);
 
-		sampleSceneShadow = Model::createFromFile("sampleScene_shadow", modelsDirectory + "/teapots-pillars/samplescene.dae",
+		lightPassMat = Material::createFromFiles("lightPass_mat",
 			{
-				shadersDirectory + "depth_map.vert",
-				shadersDirectory + "depth_map.frag"
+				shadersDirectory + "light_pass.vert",
+				shadersDirectory + "light_pass.frag"
+			},
+			{
+
 			}
 		);
-		sampleSceneShadow->setScale(glm::vec3(0.05f, 0.05f, 0.05f));
 
-		sampleScene = Model::clone("sampleScene", sampleSceneShadow.get());
-		std::shared_ptr<Material> mat = Material::createFromData("mat",
+		diffuseMat = Material::createFromData("scene_mat",
 			{
 				shadersDirectory + "scene.vert",
 				shadersDirectory + "scene.frag"
@@ -80,41 +83,51 @@ public:
 				{ "depthMap", lightMap },
 			}
 		);
-		sampleScene->setMaterial(mat);
-		sampleScene->setUniform("biasMatrix", biasMatrix);
+
+		sampleScene = Model::createFromFile("sampleScene", modelsDirectory + "/teapots-pillars/samplescene.dae", {}, false);
 		sampleScene->setScale(glm::vec3(0.05f, 0.05f, 0.05f));
 	}
 
 	virtual void render(float deltaTime) override
 	{
-		// change light position over time
-		lightPos = glm::vec3(-2.0f + sin(timePassed) * 2.0f, 5.0f + cos(timePassed) * 1.0f, 2.0f + cos(timePassed) * 1.0f);
-		glm::mat4 lightProj = glm::perspective<float>(glm::radians(45.0f), 1.0f, 1.0f, 10.0f);
-		glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0, 1.0, 0.0));
-		glm::mat4 lightSpaceMatrix = lightProj * lightView;
+		lightMapPass();
 
-		glViewport(0, 0, lightMapWidth, lightMapHeight);
-		lightMapFBO->bind();
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		sampleSceneShadow->setUniform("lightSpaceMatrix", lightSpaceMatrix);
-		// fixed peter panning, use cull front face when render scene to depth map
-		glCullFace(GL_FRONT);
-		sampleSceneShadow->render();
-		glCullFace(GL_BACK);
-
-		lightMapFBO->unbind();
 		glViewport(0, 0, mWindowWidth, mWindowHeight);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		sampleScene->setMaterial(diffuseMat);
 		sampleScene->setUniform("lightPos", lightPos);
 		sampleScene->setUniform("viewPos", mMainCamera->getPosition());
 		sampleScene->setUniform("lightSpaceMatrix", lightSpaceMatrix);
+		sampleScene->setUniform("biasMatrix", biasMatrix);
 		sampleScene->render();
 	}
 
 	virtual void windowResized() override
 	{
 		ExampleBase::windowResized();
+	}
+
+	void lightMapPass()
+	{
+		// change light position over time
+		lightPos = glm::vec3(-2.0f + sin(timePassed) * 2.0f, 5.0f + cos(timePassed) * 1.0f, 2.0f + cos(timePassed) * 1.0f);
+		glm::mat4 lightProj = glm::perspective<float>(glm::radians(45.0f), 1.0f, 1.0f, 10.0f);
+		glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0, 1.0, 0.0));
+		lightSpaceMatrix = lightProj * lightView;
+
+		glViewport(0, 0, lightMapSize, lightMapSize);
+		lightMapFBO->bind();
+		glClear(GL_DEPTH_BUFFER_BIT);
+		sampleScene->setMaterial(lightPassMat);
+		sampleScene->setUniform("lightSpaceMatrix", lightSpaceMatrix);
+		// fixed peter panning, use cull front face when render scene to depth map
+		glCullFace(GL_FRONT);
+		sampleScene->render();
+		glCullFace(GL_BACK);
+
+		lightMapFBO->unbind();
 	}
 };
 
