ccb23a69aa549bf004a5ee30a40428478ae5e0dc
linziwei <282633894@qq.com>
2019-12-10 18:22:52 +0800

    added new Image class

---
+++ common/image.cpp
@@ -0,0 +1,6 @@
+#include "image.h"
+
+namespace es
+{
+
+}
---
+++ common/image.h
@@ -0,0 +1,17 @@
+#ifndef IMAGE_H_
+#define IMAGE_H_
+
+#include <glm/glm.hpp>
+
+#include <iostream>
+#include <memory>
+
+namespace es
+{
+	class Image
+	{
+
+	};
+}
+
+#endif
